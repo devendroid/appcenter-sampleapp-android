631d96142fd851d28d65ccc4850f1e06e70c9772
IdealMac <IdealMac@Macbooks-MacBook-Pro.local>
2021-02-05 17:55:25 +0530

    Add post build sh

---
+++ appcenter-post-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo '=======POST BUILD========='
+#echo 'Step 1: Generate Basic Authorization'
+
+#BASICAUTH="$(echo -n dineshyadav:da30e7e0efeb7e9aa5ea32837a9d6869cbd9fd3d | base64)"
+#header="Authorization: Basic $BASICAUTH"
+
+#JSON="{\"filename\":\"My Application\",\"appPath\":\"${KAPPPATH}\"}"
+
+#echo
+#curl -X POST https://api.kobiton.com/v1/apps \
+#-H "$header" \
+#-H 'content-type: application/json' \
+#-d "$JSON"
+
+echo '...Done'
