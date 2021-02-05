67eeb6db0e5cfbd4126bb4b8d629b24a93a74ef6
IdealMac <IdealMac@Macbooks-MacBook-Pro.local>
2021-02-05 19:01:52 +0530

    Re locate post build sh file

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,3 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-echo "This is an App Center Post-Build script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
+echo '=======POST BUILD========='
+#echo 'Step 1: Generate Basic Authorization'
+
+#BASICAUTH="$(echo -n dineshyadav:da30e7e0efeb7e9aa5ea32837a9d6869cbd9fd3d | base64)"
+#header="Authorization: Basic $BASICAUTH"
+
+#JSON="{\"filename\":\"My Application\",\"appPath\":\"${KAPPPATH}\"}"
+
+#echo
+#curl -X POST https://api.kobiton.com/v1/apps \
+#-H "$header" \
+#-H 'content-type: application/json' \
+#-d "$JSON"
+
+echo '...Done'
